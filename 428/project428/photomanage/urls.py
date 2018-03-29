f98354fb0f213a6fb242c248105dce21e88fe8a6
Taylor Swift <qiao.song@outlook.com>
2018-03-29 14:52:28 -0400

    fix url

--- 428/project428/photomanage/urls.py
+++ 428/project428/photomanage/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns('photomanage.views',
     url(r'^$', 'index', name='home'),
     # for our home/index page
  
-    url(r'^add_category/(?P<photo_id>\d)/(?P<category_code>\d)$', 'add_category', name='addcategory'),
+    url(r'^add_category/(?P<photo_id>\d+)/(?P<category_code>\d)$', 'add_category', name='addcategory'),
     url(r'^get_all_photos/$', 'get_all_photos', name='getallphotos'),
     url(r'^get_photos_by_category/(?P<category_code>\d)$', 'get_photos_by_category', name='getphotosbycategory'),
     url(r'^reset_category/$', 'reset_category', name='resetcategory'),
